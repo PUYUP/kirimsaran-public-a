940cc6bb322909536489e3840fd6f52a5c4d1976
PUYUP <hellopuyup@gmail.com>
2021-09-23 14:36:48 +0700

    downgrade nextjs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const optimizedImages = require('next-optimized-images');
 
 const nextConfiguration = {
   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+  env: {
+    // apiHost: 'http://localhost:8000',
+    apiHost: 'https://apiks.cliptask.com',
+  }
 };
 
 module.exports = withPlugins([optimizedImages], nextConfiguration);
