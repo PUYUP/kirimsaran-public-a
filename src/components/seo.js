90985d7588663cc8a51a15f7469316f86ca44459
PUYUP <hellopuyup@gmail.com>
2021-09-24 08:09:38 +0700

    change meta

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function SEO({
-  description = 'startup product landing page',
-  author = 'RedQ, Inc',
+  description = 'Kirim saran dengan akun anonim',
+  author = 'PUYUP',
   meta,
-  title = 'startup landing title',
+  title = 'Kirim Saran',
 }) {
   const metaData = [
     {
